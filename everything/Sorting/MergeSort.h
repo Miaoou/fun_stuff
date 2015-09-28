a89ca419bdae4c4eeb95a50dd6e3ee5499ddab5c
Miaoou <miaoou@gmail.com>
2015-09-28 17:04:48 +0200

    - merge sort c functional for multpile of 2 size array

--- everything/Sorting/MergeSort.h
+++ everything/Sorting/MergeSort.h
@@ -4,34 +4,48 @@
 #ifndef MERGESORT_H
 #define MERGESORT_H
 
-void
-merge( int ar[], int arSz )
+int*
+merge( int left[], int right[], int totalSz )
 {
-    if( arSz == 1 )
-        return;
+    int* ar = new int[ totalSz ];
+    int mid = totalSz / 2;
 
-    int mid = arSz / 2;
-    static int* left = new int[ mid ];
-    static int* right = new int[ arSz - mid ];
-
-    memcpy( left, ar, mid * sizeof( int ) );
-    memcpy( right, &ar[ mid ], ( arSz - mid ) * sizeof( int ) );
-
-    for( int i = 0, j = 0; i < mid && j < ( arSz - mid ); )
+    int i = 0;
+    int j = 0;
+    while( i < mid && j < mid )
     {
-        if( j >= mid || left[ i ] < right[ j ] )
+        if( left[ i ] < right[ j ] )
             ar[ i + j ] = left[ i++ ];
-        else if( i >= mid || left[ i ] >= right[ j ] )
+        else if( left[ i ] >= right[ j ] )
             ar[ i + j ] = right[ j++ ];
     }
+
+    while( i < mid )
+        ar[ i + j ] = left[ i++ ];
+
+    while( j < mid )
+        ar[ i + j ] = right[ j++ ];
+
+    return ar;
 }
 
-template< int arSz >
-void
-merge_sort_c( int( &a )[ arSz ] )
+int*
+merge_sort_c( int ar[], int arSz )
 {
-    int ar[] = { 1, 2, 5, 3, 4, 7 };
-    merge( ar, 6 );
+    if( arSz == 1 )
+        return ar;
+
+    int mid = arSz / 2;
+    int* left = new int[ mid ];
+    int* right = new int[ mid ];
+
+    memcpy( left, ar, mid * sizeof( int ) );
+    memcpy( right, &ar[ mid ], mid * sizeof( int ) );
+    
+    left = merge_sort_c( left, mid );
+    right = merge_sort_c( right, mid );
+
+    return merge( left, right, arSz );
 }
 
 template< int arSz >
