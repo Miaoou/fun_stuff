a07ea1b15a1fa9f07262f0d762ffb8b58693a763
Miaoou <miaoou@gmail.com>
2015-09-29 10:38:48 +0200

    - Added merge sort cpp

--- everything/Sorting/Sorting.cpp
+++ everything/Sorting/Sorting.cpp
@@ -4,13 +4,17 @@
 #include "BubbleSort.h"
 #include "InsertionSort.h"
 #include "QuickSort.h"
+#include "MergeSort.h"
 
 int ar[] = { 5, 2, 4, 13, 0, 44, 0 };
 
 int
 main()
 {
-    insertion_sort_c( ar );
+    int ar1[] = { 1, 3, 5, 2, 4, 7, 0, 6 };
+    
+    //int* arSorted = merge_sort_c( ar1, sizeof( ar1 ) / sizeof( ar[ 0 ] ) );
+    merge_sort_cpp( ar1 );
 
     return 0;
 }
