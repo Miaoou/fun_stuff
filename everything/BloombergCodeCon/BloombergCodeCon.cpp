2b709320ec5a7c4d3e492f6a53d4e4618b06bc06
Miaoou <miaoou@gmail.com>
2015-10-22 16:05:15 +0200

    - tmp

--- everything/BloombergCodeCon/BloombergCodeCon.cpp
+++ everything/BloombergCodeCon/BloombergCodeCon.cpp
@@ -1,11 +1,13 @@
 // BloombergCodeCon.cpp
 
 #include "MatchingGloves.h"
+#include "ConferenceRoomScheduler.h"
 
 int
 main()
 {
-    matching_gloves();
+    //matching_gloves();
+    conference_room_scheduler();
 
     return 0;
 }
